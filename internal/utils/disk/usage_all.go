1f4244290587e328252013b0fd8b90e4fb5079aa
Gleb Zhizhchenko <zhizhchenkogm@loodsen.ru>
2024-01-19 10:49:58 +0300

    feat: add integration tests

--- internal/utils/disk/usage_all.go
+++ internal/utils/disk/usage_all.go
@@ -17,7 +17,7 @@ import (
 func (d *disk) Usage(ctx context.Context, path string) (*model.Stat, error) {
 	st, err := diskUtil.UsageWithContext(ctx, path)
 	if errors.Is(err, syscall.ENOENT) {
-		err = os.MkdirAll(path, 0666)
+		err = os.MkdirAll(path, 0700)
 		if err != nil {
 			return nil, fmt.Errorf("mkdir all: %w", err)
 		}
